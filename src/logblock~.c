75ec2f77da9114891fa4b30527a334189061dd0e
Jim Kang <jimkang@gmail.com>
2021-12-15 20:53:53 -0500

    Add a set method. Set the class args. Args still aren't being passed correctly to the patch.

--- src/logblock~.c
+++ src/logblock~.c
@@ -39,7 +39,7 @@ static void logblock_banged(t_logblock *x)
 {
   x->activate = x->activate ? 0 : 1;
   if (!x->activate) {
-    x->currentIndex = 0.0;
+    x->currentIndex = 0;
   }
   printf("activate: %d\n", x->activate);
 }
@@ -48,18 +48,19 @@ static t_int *logblock_perform(t_int *w)
 {
   t_sample *in = (t_sample *)w[1];
   t_logblock *x = (t_logblock *)w[2];
+  printf("targetIndex: %f, logname: %s\n", x->targetIndex, x->logname->s_name);
   int n = (int)w[3];
 
   if (x->activate) {
     x->currentIndex += 1;
 
-    int non_zero = 0;
-    while (n--) {
-      if (*in++ != 0.) {
-        non_zero = 1;
-        break;
-      }
-    }
+    //int non_zero = 0;
+    //while (n--) {
+      //if (*in++ != 0.) {
+        //non_zero = 1;
+        //break;
+      //}
+    //}
     if (x->currentIndex == x->targetIndex) {
       printf("This is it: %d", x->currentIndex);
     }
@@ -74,34 +75,43 @@ static void logblock_dsp(t_logblock *x, t_signal **sp)
   dsp_add(logblock_perform, 3, sp[0]->s_vec, x, sp[0]->s_n);
 }
 
+
 static void logblock_tilde_helper(void)
 {
   post("\n"HEARTSYMBOL
        " logblock~-object :: log a target block to a file");
   post("'bang'\t: turn the block counter on\n"
        "'off'\t: turn it off\n"
-       "<1/0>\t: turn it on/off\n"
+       "'targetIndex'\t: index of the block to log\n"
        "'logname'\t: the name of the log file\n"
        "'help'\t: view this\n"
        "signal~");
 }
 
-static void *logblock_new(void)
+static void logblock_set(t_logblock *x, t_float *f, t_symbol *s) {
+  x->targetIndex = *f;
+  x->logname = s;
+}
+
+static void *logblock_new(t_float *f, t_symbol *s)
 {
   t_logblock *x = (t_logblock *)pd_new(logblock_class);
-  outlet_new(&x->x_obj, gensym("float"));
+  x->targetIndex = *f;
+  x->logname = s;
   x->activate = 0;
   return (x);
 }
 
 ZEXY_SETUP void logblock_tilde_setup(void)
 {
+  // TODO: dtor
   logblock_class = zexy_new("logblock~",
-                           logblock_new, 0, t_logblock, 0, "");
+                           logblock_new, 0, t_logblock, CLASS_DEFAULT, "fs");
   class_addbang(logblock_class, logblock_banged);
 
   zexy_addmethod(logblock_class, (t_method)nullfn, "signal", "");
   zexy_addmethod(logblock_class, (t_method)logblock_dsp, "dsp", "!");
+  zexy_addmethod(logblock_class, (t_method)logblock_set, "set", "s");
 
   zexy_addmethod(logblock_class, (t_method)logblock_tilde_helper, "help", "");
   zexy_register("logblock~");
